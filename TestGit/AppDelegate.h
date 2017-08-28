b07ca46a96e4089cb8fdbaa76023eaf28d9566c6
CoderBruce <nortonaccount123@126.com>
2017-08-28 12:35:43 +0800

    初始化

---
+++ TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by Bruce Tian on 2017/8/28.
+//  Copyright © 2017年 Bruce Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by Bruce Tian on 2017/8/28.
+//  Copyright © 2017年 Bruce Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
